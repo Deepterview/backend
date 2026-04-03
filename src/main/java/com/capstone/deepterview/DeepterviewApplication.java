c60347e7733490284c983ce6b3ccc4d6ff41439f
HarryBae1011 <06harry@naver.com>
2026-04-04 01:41:07 +0900

    settings

---
+++ src/main/java/com/capstone/deepterview/DeepterviewApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.deepterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepterviewApplication.class, args);
+	}
+
+}
