9cd625479b39b62d385392d957a0a8c2f34f932c
HarryBae1011 <06harry@naver.com>
2026-05-08 15:01:41 +0900

    컨트롤러 Tag 추가

--- src/main/java/com/capstone/deepterview/domain/report/controller/ReportController.java
+++ src/main/java/com/capstone/deepterview/domain/report/controller/ReportController.java
@@ -4,6 +4,7 @@ import com.capstone.deepterview.domain.interview.dto.response.SessionReportRespo
 import com.capstone.deepterview.domain.interview.service.InterviewService;
 import com.capstone.deepterview.domain.member.dto.response.UserPrincipal;
 import com.capstone.deepterview.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "피드백 리포트 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/sessions")
